6974dfac2c843a9f3faa40896096b967894665fc
Theodore Book <theodorebook@gmail.com>
2012-10-20 16:31:09 -0500

    Initial spider

---
+++ serversurvey/serversurvey/spiders/SurveySpider.py
@@ -0,0 +1,60 @@
+import csv
+
+from scrapy.spider import BaseSpider
+
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from serversurvey.items import ServersurveyItem
+
+class SurveySpider(BaseSpider):
+    """
+    Reads in a list of URLS and retrieves server software versions.
+    """
+    name = 'surveyspider'
+    allowed_domains = ['rice.edu', 'gatech.edu']
+    start_urls = ['http://www.rice.edu/', 'http://gatech.edu']
+
+    def __init__(self):
+        BaseSpider.__init__(self)
+        
+        print 'Opening Alexa URL CSV, please wait.'
+        maxSites = 500
+        
+        csv_file = open('top-1m.csv','r') 
+        reader = csv.reader(csv_file)
+        
+        urls = []
+        rank=0
+        while(rank < maxSites):
+            domain = reader.next()[1]
+            self.allowed_domains.append( domain )
+            urls.append( 'http://www.' + domain ) 
+            rank += 1
+        
+        self.start_urls += urls
+        
+        csv_file.close()
+        print 'Done opening URLs, starting crawler....'
+
+
+    def parse(self, response):
+        """
+        Parses stuff
+        """
+        # we want to grab the server versions from responses
+        print response.url
+        print response.headers
+        print response.headers.get('Server'), response.headers.get('Content-Type')
+        
+        # may want to compare server daye to our own here and save it in items
+        
+        item = ServersurveyItem()
+        item['url'] = response.url
+        item['version'] = response.headers.get('Server')
+        item['contentType'] = response.headers.get('Content-Type')
+        item['header'] = response.headers.__str__()
+        
+        # things to do: - figure how to store and analyze data from crawler
+        #               - figure our a way to ask for features on the server
+        #               - figure out a way to test server version via behavior (bugs)
+        
+        return item
