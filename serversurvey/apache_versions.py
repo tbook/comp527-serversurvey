d3f6dae1590d55e58e65b49ac00fb7da3d8fba78
Martha Witick <mmwitick@gmail.com>
2012-11-29 15:25:18 -0600

    added probabilistic analysis over apache versions, not very effective though

--- serversurvey/apache_versions.py
+++ serversurvey/apache_versions.py
@@ -7,131 +7,7 @@ import re
 from operator import itemgetter
 
 # globals #
-version_regex = re.compile("\d+([.][0-9a-zA-Z]+)*")
-
-"""
-# probabilistic methods #
-def train_probabilities(filename):
-    '''
-    When given the name of a CSV, returns
-    '''
-    # load training data
-    csvfile = file(filename,"r")
-    reader = csv.reader(csvfile)
-
-    #Data
-    headerIndices = {}  #Index for each field in input files
-    versionResponseCounts = {}  #Dict of sets, containing all the responses for each version
-    versionCounts = {}   #The number of sites reporting each version type
-    responseCounts = {} #The total count of each response
-    siteVersions = {}    #Dict giving the version type for each site
-    totalCount = 0      #The total number of sites
-
-    # grab the first row and use it as the header
-    indices = {}
-    header = reader.next()
-    for i,name in enumerate(header):
-        indices[name] = i
-
-    #
-    for row in reader:
-        raw_version = row[indices['version']]
-        
-        #Generate the response code
-        responseCode = str(row[ indices['requestType']]) + str(row[ indices['status']])
-
-        siteName = row[headerIndices['requestUrl']]
-            
-        #Ignoring non-apache and empty versions, store:
-        if (is_apache( raw_version ) and not get_version(raw_version) == ""):
-            thisVersionResponses = versionResponseCounts.get(versionName, {})
-            thisResponseCount = thisVersionResponses.get(responseCode, 0)
-            thisResponseCount = thisResponseCount + 1
-            thisVersionResponses[responseCode] = thisResponseCount
-            versionResponseCounts[versionName] = thisVersionResponses
-            
-            thisResponseCount = responseCounts.get(responseCode, 0)
-            thisResponseCount = thisResponseCount + 1
-            responseCounts[responseCode] = thisResponseCount
-            
-            siteVersions[siteName] = versionName
-
-    #Calculate statistics on the dataset
-    for site in siteVersions.keys() :
-        totalCount = totalCount + 1
-        thisVersion = siteVersions[site]
-        thisVersionCount = versionCounts.get(thisVersion, 0)
-        thisVersionCount = thisVersionCount + 1
-        versionCounts[thisVersion] = thisVersionCount
-
-    # return
-
-def guess_apache_versions(loadfile, training):
-    '''
-    When given a CSV of server data to load and the training above, assign
-    versions to Apache servers with certainty and save the result.
-    '''
-
-    # load data
-    csvfile = file(loadfile,"r")
-    reader = csv.reader(csvfile)
-    reader.next()   #Dump the headers
-    
-    #Build a dict with all of the responses and versions for each site    
-    site_response_dict = {}   #Dict of lists of all responses for a given site
-    reported_version = {}     #Reported version from a single site
-    for row in reader:
-        url = row[ headerIndices['requestUrl'] ]
-        responce = str(row[ headerIndices['requestType']]) \
-            + str(row[ headerIndices['status']])
-        siteResponses = site_response_dict.get(url, [])
-        siteResponses.append(responce)
-        site_response_dict[url] = siteResponses
-        
-        # Get reported version
-        version = get_version( row[ headerIndices['version'] ] )
-        reported_version[url] = version
-    
-    #Predict the version for each site
-    finalProbs = {}     #Dict of dicts - each included dict is probability for a given version
-    
-    for site in site_response_dict.keys() :
-        versionFinalProbs = {}
-        for server in serverCounts.keys() :
-            pProductServer = 1.0
-            pProductNotServer = 1.0
-            for response in site_response_dict[site] :
-                pResponseGivenServer = float(serverResponseCounts[server].get(response,0)) /\
-                        serverCounts[server]
-                pProductServer = pProductServer * pResponseGivenServer
-                pResponseNotServer = float(responseCounts.get(response,0) - serverResponseCounts[server].get(response,0)) / (totalCount - serverCounts.get(server,0))
-                pProductNotServer = pProductNotServer * pResponseNotServer
-            pServer = float(serverCounts[server]) / totalCount
-            pNotServer = float(totalCount - serverCounts[server]) / totalCount
-            denominator = pServer * pProductServer + pNotServer * pProductNotServer
-            if (denominator == 0):
-                print("Invalid data for server " + server + " on site " + site)
-            else:
-                pThisServer = pServer * pProductServer / denominator
-                versionFinalProbs[server] = pThisServer
-        finalProbs[site] = versionFinalProbs
-        
-    #Write the results        
-    outfile = args.o[0]
-    writer = csv.writer(file(outfile, 'w+'), delimiter='\t')
-    
-    #print a header
-    writer.writerow(["Site","Reported Server","Calculated Server","Probability"])
-    
-    for site in finalProbs.keys():
-        likelyServer = "unknown"
-        serverProbability = 0.0
-        for server in finalProbs[site].keys():
-            if (finalProbs[site][server] > serverProbability):
-                likelyServer = server
-                serverProbability = finalProbs[site][server]
-        writer.writerow([site, reported_version[site], likelyServer, serverProbability])
-"""
+version_regex = re.compile("\d+([.][0-9xX]+)*")
 
 # counting methods #
 def is_apache(server_string):
@@ -216,19 +92,33 @@ def main(argv):
     parser = argparse.ArgumentParser(description="Count apache version strings and write results to output.")
     parser.add_argument("-i", nargs=1, help="input file")
     parser.add_argument("-t", nargs=1, help="training file")
-    parser.add_argument("-o", nargs=1, help="output file")
+    parser.add_argument("-o", nargs=1, help="output file for version counts")
+    parser.add_argument("-O", nargs=1, help="output file for probabilistic")
     args = parser.parse_args()
 
     input = "survey-bottom-10k.csv"
     output = "apache_version_counts.csv"
+    training = input
+    poutput = "apache_version_probabilities.csv"
+    do_probabilistic = False
     if args.i != None: input = args.i[0]
     if args.o != None: output = args.o[0]
-    #training = args.t[0]
+    if args.t != None:
+        do_probabilistic = True
+        training = args.t[0]
+    if args.O != None: poutput = args.O[0]
+
+    # if probabilistic, do probabilisitc analysis
+    if do_probabilistic:
+        (versionCounts, responseCounts, versionResponseCounts, totalCount) = train_probabilities(training)
+        guess_apache_versions(input, poutput, versionCounts, responseCounts, versionResponseCounts, totalCount)
+
+        
 
     # Read in versions
     apache_versions = load_version_strings(input)
 
-    # Write results
+    # Write version count results
     print apache_versions
     sorted_keys = sorted(apache_versions)
 
